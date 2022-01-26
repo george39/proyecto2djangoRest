743814af07fde73d6af2476676532fe31ffd5acd
george39 <george@utp.edu.co>
2022-01-25 21:07:11 -0500

    agenda con django restframework

---
+++ tienda/applications/users/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'users_app'
+
+urlpatterns = [
+    path(
+        'login/',
+         views.LoginUser.as_view(), name='login'
+    ),
+]
